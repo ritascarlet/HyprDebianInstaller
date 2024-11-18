cb619d59f9b73ff143c04c48be307d96317be9f7
rita <137996547+ritascarlet@users.noreply.github.com>
2024-11-18 17:58:21 +0300

    Update NvidiaCudaInstaller.sh

--- NvidiaCudaInstaller.sh
+++ NvidiaCudaInstaller.sh
@@ -1,6 +1,11 @@
 sudo apt install linux-headers-$(uname -r) build-essential dkms curl wget
 
+cd
+
 wget https://developer.download.nvidia.com/compute/nvidia-driver/560.35.03/local_installers/nvidia-driver-local-repo-debian12-560.35.03_1.0-1_amd64.deb
 
 sudo dpkg -i nvidia-driver-local-repo-debian12-560.35.03_1.0-1_amd64.deb
 
+sudo cp /var/nvidia-driver-local-repo-debian12-560.35.03/nvidia-driver-local-8192D793-keyring.gpg /usr/share/keyrings/
+
+sudo apt-get install -y nvidia-open-560
