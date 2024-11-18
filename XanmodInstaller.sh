0770ad7bcd2a366507576b18ed3e44a3e5abe174
rita <137996547+ritascarlet@users.noreply.github.com>
2024-11-18 17:16:23 +0300

    Delete XanmodInstaller.sh

--- XanmodInstaller.sh
+++
@@ -1,4 +0,0 @@
-wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -vo /usr/share/keyrings/xanmod-archive-keyring.gpg
-
-echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-release.list
-
