7bf36286cb69efaaf1a075f5b1e6b5d1f1fad0cb
rita <137996547+ritascarlet@users.noreply.github.com>
2024-11-19 16:11:26 +0300

    Update HyprDebian.sh

--- HyprDebian.sh
+++ HyprDebian.sh
@@ -39,8 +39,7 @@ apt update
 
 apt upgrade -y
 
-apt-get install -y meson wget build-essential ninja-build edid-decode cmake-extras cmake gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev libdrm-dev libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev libudev-dev libseat-dev seatd libxcb-dri3-dev libvulkan-dev libvulkan-volk-dev  vulkan-utility-libraries-dev libvkfft-dev libgulkan-dev libegl-dev libgles2 libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-xinput-dev xdg-desktop-portal-hyprland hwdata libpugixml-dev libzip-dev librsvg2-dev libtomlplusplus-dev libgbm-dev libxcursor-dev libxcb-errors-dev xwayland polkitd libnvidia-egl-wayland1 libnvidia-egl-gbm1 libdisplay-info2 libliftoff0 libudis86-0 gcc clang
-
+apt-get install -y meson wget build-essential ninja-build cmake-extras cmake gettext gettext-base fontconfig libfontconfig-dev libffi-dev libxml2-dev libdrm-dev libxkbcommon-x11-dev libxkbregistry-dev libxkbcommon-dev libpixman-1-dev libudev-dev libseat-dev seatd libxcb-dri3-dev libvulkan-dev libvulkan-volk-dev  vulkan-utility-libraries-dev libvkfft-dev libgulkan-dev libegl-dev libgles2 libegl1-mesa-dev glslang-tools libinput-bin libinput-dev libxcb-composite0-dev libavutil-dev libavcodec-dev libavformat-dev libxcb-ewmh2 libxcb-ewmh-dev libxcb-present-dev libxcb-icccm4-dev libxcb-render-util0-dev libxcb-res0-dev libxcb-xinput-dev libpugixml-dev libzip-dev librsvg2-dev libtomlplusplus-dev libgbm-dev libxcursor-dev libxcb-errors-dev xwayland polkitd libnvidia-egl-wayland1 libnvidia-egl-gbm1 libliftoff0 libudis86-0 gcc clang hwdata libglm-dev libbenchmark-dev liblcms2-dev libxmu-dev libxcursor-dev libeis-dev libpixman-1-dev libx11-dev libxdamage-dev libxcomposite-dev libxrender-dev libxext-dev libxfixes-dev libxxf86vm-dev libxtst-dev libxres-dev libdrm-dev libvulkan-dev libxkbcommon-dev libsdl2-dev libpipewire-0.3-dev libavif-dev libwlroots-dev libliftoff-dev libcap-dev spirv-headers libstb-dev libdecor-0-dev libxdamage-dev git glslang-tools vulkan-tools libexpat1-dev libxml2-dev graphviz doxygen xsltproc xmlto 
 
 echo ""
 echo ""
@@ -63,6 +62,32 @@ mkdir HyprDebian
 cd HyprDebian
 
 echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "-----------------------------------"
+echo -e "\e[32mBild edid-decode\e[0m"
+echo "-----------------------------------"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""  
+
+git clone git://linuxtv.org/edid-decode.git
+
+cd edid-decode
+
+meson setup build
+
+meson compile -C build
+
+meson install -C build
+
+cd ../..
+
+echo ""
 echo ""   
 echo ""
 echo ""
@@ -75,22 +100,21 @@ echo ""
 echo ""
 echo ""
 echo ""
-git clone https://gitlab.freedesktop.org/wayland/wayland
 
-cd wayland
+wget https://gitlab.freedesktop.org/wayland/wayland/-/releases/1.23.1/downloads/wayland-1.23.1.tar.xz
 
-mkdir build
-cd    build
+mkdir build &&
+cd    build &&
 
 meson setup ..            \
       --prefix=/usr       \
-      --buildtype=release \
-      -Ddocumentation=false
+      --buildtype=release &&
 ninja
 
 ninja install
 
 cd ../..
+
 echo ""
 echo ""   
 echo ""
@@ -104,17 +128,18 @@ echo ""
 echo ""
 echo ""
 echo ""
-git clone https://gitlab.freedesktop.org/wayland/wayland-protocols.git
 
-cd wayland-protocols
+wget https://gitlab.freedesktop.org/wayland/wayland-protocols/-/releases/1.38/downloads/wayland-protocols-1.38.tar.xz
 
-mkdir build
-cd    build
+tar -xvJf wayland-protocols-1.38.tar.xz
 
-meson setup --prefix=/usr --buildtype=release
+cd wayland-protocols-1.38
 
+mkdir build &&
+cd    build &&
+
+meson setup --prefix=/usr --buildtype=release &&
 ninja
-
 
 ninja install
 
@@ -134,20 +159,47 @@ echo ""
 echo ""
 echo ""
 
-git clone https://gitlab.freedesktop.org/emersion/libdisplay-info.git
+wget https://gitlab.freedesktop.org/emersion/libdisplay-info/-/releases/0.2.0/downloads/libdisplay-info-0.2.0.tar.xz
 
-cd libdisplay-info
+tar -xvJf libdisplay-info-0.2.0.tar.xz
 
-mkdir build
-cd    build
+cd libdisplay-info-0.2.0
 
-meson setup --prefix=/usr --buildtype=release
+meson setup build/
 
-ninja
+ninja -C build/
 
-ninja install
+ninja -C build/ install
 
-cd ../..
+cd ..
+
+echo ""
+echo ""   
+echo ""
+echo ""   
+echo ""
+echo "-----------------------------------"
+echo -e "\e[32mInstall Wlroots\e[0m"
+echo "-----------------------------------"
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+
+wget https://gitlab.freedesktop.org/wlroots/wlroots/-/releases/0.18.1/downloads/wlroots-0.18.1.tar.gz
+
+tar -xvf wlroots-0.18.1.tar.gz
+
+cd wlroots-0.18.1
+
+meson setup build/
+
+ninja -C build/
+
+sudo ninja -C build/ install
+
+cd ..
 
 echo ""
 echo ""   
@@ -190,7 +242,8 @@ echo ""
 echo ""
 
 git clone https://github.com/hyprwm/hyprutils.git
-cd hyprutils/
+
+cd hyprutils
 
 cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build
 
@@ -292,9 +345,11 @@ echo ""
 echo ""
 echo ""
 
-git clone --recursive https://github.com/hyprwm/Hyprland
+wget https://github.com/hyprwm/Hyprland/releases/download/v0.45.1/source-v0.45.1.tar.gz
 
-cd Hyprland
+tar -xvf source-v0.45.1.tar.gz
+
+cd hyprland-source 
 
 make all  
 
